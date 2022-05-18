d4d29df4f2cf2f11bf4126ac1b6a2becf6a0b2af
Diego Barros <diegobarrosaraya@outlook.com>
2022-05-17 20:43:05 -0400

    "commit inicial"

---
+++ shell.nix
@@ -0,0 +1,13 @@
+# shell.nix
+##Este archivo es para automatizar mi entorno de desarrollo en nixOS
+#--Diego--
+{ pkgs ? import <nixpkgs> {} }:
+let
+  python-with-my-packages = pkgs.python3.withPackages (p: with p; [
+    pandas
+    requests
+    django
+    # other python packages you want
+  ]);
+in
+python-with-my-packages.env # replacement for pkgs.mkShell
