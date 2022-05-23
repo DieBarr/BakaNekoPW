ba028097dcb0b88c850631574d602e1d59f68911
PanConQueso0 <ja.pastenes@duocuc.cl>
2022-05-23 15:50:38 -0400

    se agregaron tablas al models

--- bakaNeko/models.py
+++ bakaNeko/models.py
@@ -8,9 +8,28 @@ class Usuario(models.Model):
     nombreUsuario = models.CharField(max_length=30, verbose_name="Nombre de Usuario")
     contrasenia = models.CharField(max_length=20, verbose_name="Contraseña del Usuario")
     email = models.EmailField(max_length=30, verbose_name="Correo del Usuario")
-
-class Perfil(models.Model):
     foto_perfil = models.ImageField(upload_to="fotoPerfiles", null=True)
-    pais_perfil = models.CharField(max_length=20, verbose_name="Pais del Usuario")
     tipo_user = models.CharField(max_length=5, verbose_name="Tipo de Usuario") # Admin | Comun | Mod
-    usuario = models.ForeignKey(Usuario, on_delete = models.CASCADE)
+
+class Estado(models.Model):
+    idStatus = models.AutoField(primary_key=True, verbose_name="Codigo de Status")
+    nombre = models.CharField(max_length=30, verbose_name="Tipo de status")
+class Post(models.Model):
+    idPost = models.AutoField(primary_key=True, verbose_name="Codigo del Post")
+    fechaPost = models.DateField(verbose_name="Fecha del Post")
+    tituloPost = models.CharField(max_length=150, verbose_name="Titulo del Post")
+    descPost = models.CharField(max_length=500, verbose_name="Descripcion del Post")
+    imagenPost = models.ImageField(upload_to="imagenPosts", null=True)
+    estado = models.ForeignKey(Estado)
+    razonPost = models.CharField(max_length=100, null=True, verbose_name="Razon del Baneo")
+    usuario = models.ForeignKey(Usuario)
+
+class Comentario(models.Model):
+    idCom = models.AutoField(primary_key=True, verbose_name="Codigo del Comentario")
+    fechaCom = models.DateField(verbose_name="Fecha del comentario")
+    usuario = models.ForeignKey(Usuario)
+    post = models.ForeignKey(Post)
+    estado = models.ForeignKey(Estado)
+    razonCom = models.CharField(max_length=100, null=True, verbose_name="Razon del Baneo")
+    descCom = models.CharField(max_length=400, verbose_name="Contenido del comentario")
+
--- bakaNeko/views.py
+++ bakaNeko/views.py
@@ -3,3 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request,'bakaNeko/index.html')
+
+
+
+def listado(request):
+    posts = Mascota.objects.all() #generando un select * from de la tabla posts
+    contexto = {"mascota":mascotas}
+    return render(request,'core/ListadoMascotas.html',contexto)
