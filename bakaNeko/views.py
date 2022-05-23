039f501eed1f1176f0d50c24750467df55a5d1d2
PanConQueso0 <ja.pastenes@duocuc.cl>
2022-05-23 15:52:34 -0400

    se agregaron tablas al models

--- bakaNeko/views.py
+++ bakaNeko/views.py
@@ -4,9 +4,3 @@ from django.shortcuts import render
 def index(request):
     return render(request,'bakaNeko/index.html')
 
-
-
-def listado(request):
-    posts = Mascota.objects.all() #generando un select * from de la tabla posts
-    contexto = {"mascota":mascotas}
-    return render(request,'core/ListadoMascotas.html',contexto)
