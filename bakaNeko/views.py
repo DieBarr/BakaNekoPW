ea71821c79d111491039ff5bfe786cb1355572d4
GonzaBah <go.bahamondes@duocuc.cl>
2022-05-25 16:26:02 -0400

    listar posts con comentarios casi listo

--- bakaNeko/views.py
+++ bakaNeko/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
-
+from .models import Post, Comentario
 # Create your views here.
 def index(request):
     return render(request,'bakaNeko/index.html')
+
+def lista(request):
+    posts = Post.objects.all()
+    coments = Comentario.objects.all()
+    contexto = {"post":posts, "comentario":coments}
+    return render(request, 'bakaNeko/listaPosts.html', contexto)
