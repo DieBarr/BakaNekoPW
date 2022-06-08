fbaa0c079c60d3746481927d1a6ff4353ad4db9f
GonzaBah <go.bahamondes@duocuc.cl>
2022-06-08 02:33:58 -0400

    cambios sustanciales favor de no cambiar

--- bakaNeko/views.py
+++ bakaNeko/views.py
@@ -11,17 +11,6 @@ def index(request):
     }
     return render(request,'bakaNeko/index.html', contexto)
 
-def index_l(request, user):
-    postsl_car = Post.objects.filter(fechaPost = datetime.date.today())
-    postsl_i = Post.objects.all()
-    usuario_l = Usuario.objects.get(idUsuario = user)
-    contexto = {
-        "postCarr":postsl_car,
-        "post":postsl_i,
-        "user":usuario_l
-    }
-    return render(request, 'bakaNeko/indexUser.html', contexto)
-
 def lista(request):
     posts = Post.objects.all()
     coments = Comentario.objects.all()
@@ -31,49 +20,25 @@ def lista(request):
 def registro(request):
     return render(request,'bakaNeko/registro.html')
   
-def registrar(request):
-    u_nombre = request.POST['nomUsuario']
-    u_email = request.POST['correoUsuario']
-    u_foto = request.FILES['fotoPerfil']
-    u_contrasenia = request.POST['contraUsuario']
-    u_repcontra = request.POST['repetirContra']
-    rol_u = Rol.objects.get(nombreRol = 'usuario')
-    ##regexEmail = "/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/"
-    ##regexPassword = "/(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}/"
-
-    if (len(u_nombre) < 4 and len(u_nombre) > 12):
-        messages.error(request, "El nombre de usuario no es válido (＃`Д´) !")
-        return redirect('registro')
-    elif u_contrasenia != u_repcontra:
-        messages.error(request, "Las contraseñas no coinciden ٩(╬ʘ益ʘ╬)۶ !")
-        return redirect('registro')
-
-    ##insert
-    Usuario.objects.create(nombreUsuario = u_nombre, email = u_email, fotoUsuario = u_foto, contrasenia = u_contrasenia, rol = rol_u)
-    return redirect('registro')
-
+def registrarUsuario(request):
+    pass
 def login(request):
-    nombre_l = request.POST['nomLogin']
-    contra_l = request.POST['contraLogin']
-   # try:
-    usuario_l = Usuario.objects.get(nombreUsuario = nombre_l)
-    contexto = {"user": usuario_l}
-    if usuario_l.contrasenia == contra_l:
-        messages.success(request, "Bienvenido "+usuario_l.nombreUsuario+" ☆*:.｡.o(≧▽≦)o.｡.:*☆!!!")
-        return render(request, 'bakaNeko/indexUser.html', contexto)
-    else:
-        messages.error(request, "La contraseña no es válida (＃`Д´)!!")
-        return redirect('registro')
-    #except:
-     #   messages.error(request, "El usuario no existe, se sugiere crear uno (╬ Ò﹏Ó)")
-      #  return redirect('registro')
+    pass
 
-
-def verPost(request, id, user):
+def verPerfil(request, id):
+    userPerf = Usuario.objects.get(idUsuario = id)
+    postPerf = Post.objects.filter(usuario_id = id)
+    contexto ={
+        "usuario": userPerf,
+        "post": postPerf
+    }
+    return render(request, 'bakaNeko/perfil.html', contexto)
+    
+def verPost(request, id):
     postSel = Post.objects.get(idPost = id)
     userSel = Usuario.objects.get(idUsuario = postSel.usuario.idUsuario)
     comSel = Comentario.objects.filter(post = postSel)
-    userAct = Usuario.objects.get(idUsuario = user)
+    userAct = Usuario.objects.get(idUsuario = 4)
     contexto = {
         "post" : postSel,
         "usuario" : userSel,
@@ -101,6 +66,7 @@ def registrarPost(request, user):
     tipo_p2 = Tipo.objects.get(idTipo = tipo_p)
     usuario_p = Usuario.objects.get(idUsuario = user)
     est_p = Estado.objects.get(nombre="activo")
+    request.session['user'] = usuario_p
     if len(titulo_p) > 100:
         messages.error(request, "Error: El Asunto no puede tener más de 100 caracteres (╬ Ò﹏Ó)!")
         return redirect('nuevoPost', usuario_p)
@@ -109,12 +75,11 @@ def registrarPost(request, user):
             img_p = request.FILES['imgPost']
             Post.objects.create(fechaPost=fecha_p, tituloPost=titulo_p, descPost=desc_p, imagenPost=img_p, estado=est_p, usuario=usuario_p, tipo=tipo_p2)
             messages.error(request, "Post creado correctamente felicidades ☆*:.｡.o(≧▽≦)o.｡.:*☆!")
-            contexto = {"user":usuario_p}
-            return render(request, 'bakaNeko/index.html', contexto)
+            return redirect('index')
         except:
             Post.objects.create(fechaPost=fecha_p, tituloPost=titulo_p, descPost=desc_p, estado=est_p, usuario=usuario_p,  tipo=tipo_p2)
             messages.success(request, "Post creado correctamente felicidades ☆*:.｡.o(≧▽≦)o.｡.:*☆!")
-            return render(request, 'bakaNeko/index.html', contexto)
+            return redirect('index')
 
 def registrarComentario(request, id, user):
     desc_c = request.POST['comment']
@@ -130,7 +95,7 @@ def registrarComentario(request, id, user):
     return redirect('verPosts', id)
 
 def secanime(request):
-    posts = Post.objects.all()
+    posts = Post.objects.filter(tipo_id = 1)
 
     datos = {
     'posts' : posts
@@ -139,7 +104,7 @@ def secanime(request):
 
 def secjuegos(request):
     
-    posts = Post.objects.all()
+    posts = Post.objects.filter(tipo_id = 2)
 
     datos2 = {
     'posts' : posts
