a5e01d3c998d80a00d177c01765f05372458db57
PanConQueso0 <101284386+PanConQueso0@users.noreply.github.com>
2022-07-06 21:12:38 -0400

    Api completada
    Completamente funcional los 3 serializadores con sus respectivos métodos y autorizaciones

--- api_rest/serializers.py
+++ api_rest/serializers.py
@@ -1,7 +1,17 @@
 from rest_framework import serializers
-from bakaNeko.models import Post
+from bakaNeko.models import Post, Comentario, Usuario
 
 class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
         fields =['idPost','fechaPost', 'tituloPost', 'descPost', 'imagenPost', 'estado', 'razonPost', 'usuario', 'tipo']
+
+class ComSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comentario
+        fields = ['idCom', 'fechaCom', 'usuario', 'post', 'estado', 'razonCom', 'descCom']
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Usuario
+        fields = ['email', 'user_name', 'profile_pic', 'register_date', 'is_staff', 'id']
